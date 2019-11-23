864636ad31a3cce9afea27ff503c05e069008bc0
Chris Morgan <chris.c.morgan.email@gmail.com>
2019-11-23 13:52:52 +1100

    fixing error on packer file location

--- scripts/confluence-install.sh
+++ scripts/confluence-install.sh
@@ -7,27 +7,20 @@ sudo mkdir /opt/data
 sudo chown ec2-user:ec2-user /opt/data
 cd /opt
 
-sudo wget https://product-downloads.atlassian.com/software/jira/downloads/atlassian-confluence-6.13.9.tar.gz
-sudo tar -zxvf atlassian-confluence-6.12.0.tar.gz
-sudo chown -R ec2-user:ec2-user atlassian-confluence-6.12.0
+sudo wget -O confluence.tar.gz https://product-downloads.atlassian.com/software/confluence/downloads/atlassian-confluence-7.0.4.tar.gz
+sudo tar -zxvf confluence.tar.gz
+sudo chown -R ec2-user:ec2-user atlassian-confluence-7.0.4
 
-echo -e "\nconfluence.home=/opt/data"  >> /opt/atlassian-confluence-6.12.0/confluence/WEB-INF/classes/confluence-init.properties
+echo -e "\nconfluence.home=/opt/data"  >> /opt/atlassian-confluence-7.0.4/confluence/WEB-INF/classes/confluence-init.properties
 
-sudo wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.34.tar.gz
-sudo tar -zxvf mysql-connector-java-5.1.34.tar.gz
-sudo chown -R ec2-user:ec2-user mysql-connector-java-5.1.34
-cp mysql-connector-java-5.1.34/mysql-connector-java-5.1.34-bin.jar atlassian-confluence-6.12.0/lib
-
-sudo mv /opt/atlassian-confluence-6.12.0/conf/server.xml /opt/atlassian-confluence-6.12.0/conf/server.xml.bck
-sudo cp /tmp/server.xml /opt/atlassian-confluence-6.12.0/conf/server.xml
+sudo mv /opt/atlassian-confluence-7.0.4/conf/server.xml /opt/atlassian-confluence-7.0.4/conf/server.xml.bck
+sudo cp /tmp/server.xml /opt/atlassian-confluence-7.0.4/conf/server.xml
 
 sudo cp /tmp/conflunence /etc/init.d/confluence
 sudo chmod +e /etc/init.d/confluence
 sudo chown ec2-user:ec2-user /opt/data -R
-sudo chown ec2-user:ec2-user /opt/atlassian-confluence-6.12.0 -R
+sudo chown ec2-user:ec2-user /opt/atlassian-confluence-7.0.4 -R
 
 sudo /etc/init.d/confluence start
 
-sudo rm -rf /opt/mysql-connector-java-5.1.34.tar.gz
-sudo rm -rf /opt/mysql-connector-java-5.1.34
-sudo rm -rf /opt/atlassian-confluence-6.12.0.tar.gz
+sudo rm -rf /opt/atlassian-confluence-7.0.4.tar.gz
